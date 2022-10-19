c21556b3dd8c51159450c276e00ffa97c583c3b1
王志森 <im.wongch1sum@gmail.com>
2022-10-19 15:10:04 +0800

    replace fetch to use axios

--- src/hooks/useBlobUrl.ts
+++ src/hooks/useBlobUrl.ts
@@ -1,29 +1,40 @@
 import { useEffect, useState } from 'react';
-
-const headers = new Headers();
-headers.append('Access-Control-Allow-Origin', '*');
-headers.append('Content-Type', 'application/json;charset=UTF-8');
+import axios from 'axios';
+// const headers = new Headers();
+// headers.append('Access-Control-Allow-Origin', '*');
+// headers.append('Content-Type', 'application/json;charset=UTF-8');
 
 // 请求url，转化为blob和附件的url
 export function useBlobUrl(link: string) {
   const [url, setUrl] = useState<null | string>(null);
   const [blob, setBlob] = useState<Blob | null>(null);
 
+  const handleFetch = () => {
+    axios(link,{responseType:"blob"}).then((response) => {
+      setBlob(response.data)
+      const result = window.URL.createObjectURL(response.data)
+      setUrl(result)
+    }).catch (error => {
+      console.log('error', error)
+    }) 
+  }
+
   useEffect(() => {
-    link &&
-      fetch(link, {
-        mode: 'no-cors',
-        headers: headers,
-        credentials: 'include',
-      })
-        .then((response) => {
-          return response.blob();
-        })
-        .then((blobResponse) => {
-          setBlob(blobResponse);
-          const result = window.URL.createObjectURL(blobResponse);
-          setUrl(result);
-        });
+    link && handleFetch()
+    // link &&
+    //   fetch(link, {
+    //     mode: 'no-cors',
+    //     headers: headers,
+    //     credentials: 'include',
+    //   })
+    //     .then((response) => {
+    //       return response.blob();
+    //     })
+    //     .then((blobResponse) => {
+    //       setBlob(blobResponse);
+    //       const result = window.URL.createObjectURL(blobResponse);
+    //       setUrl(result);
+    //     });
   }, [link]);
 
   return {
